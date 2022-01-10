ce9e7cdb7bac012634d665d3a4e15bbe53a259a5
mayur-argade <argademayur2002@gmail.com>
2022-01-10 23:45:24 +0530

    recursion intro

--- Recursion/src/com/mayur/Main.java
+++ Recursion/src/com/mayur/Main.java
@@ -3,6 +3,13 @@ package com.mayur;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello mayur great start");
+        num(0);
+    }
+    static void num(int num){
+        if(num>=5){
+            return ;
+        }
+        System.out.println(num);
+        num(num+1);
     }
 }
